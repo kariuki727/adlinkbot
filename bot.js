419a88e0e60a00e161cde3f22022dd415851ed9f
KVO <121897808+kariuki727@users.noreply.github.com>
2025-04-02 02:46:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.redirect('https://snipn.cc/');
 });
 
 const port = 8000;
