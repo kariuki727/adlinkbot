b9f2d0f61b0782c8ae8e7e6c87399116962115f4
KVO <121897808+kariuki727@users.noreply.github.com>
2025-07-26 05:21:42 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kevinnadar22/URL-Shortener-V2.git /URL-Shortener-V2
+  git clone https://github.com/kariuki727/adlinkbot.git /URL-Shortener-V2
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /URL-Shortener-V2
+  git clone $SOURCE_CODE /adlinkbot
 fi
-cd /URL-Shortener-V2
+cd /adlinkbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
